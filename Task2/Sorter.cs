867eec24597c799f6c5dae3b90050677b812304b
Alexander <alex.dem.v@gmail.com>
2017-03-12 18:57:17 +0300

    Add tests. Add comparison delegate.

--- Task2/Sorter.cs
+++ Task2/Sorter.cs
@@ -15,68 +15,83 @@ namespace Task2
         #region Public Methods
 
         /// <summary>
-        /// Sorts the elements in a one-dimensional Array using the specified IComparer.
+        /// Sorts the elements in an System.Array using the specified System.Comparison.
         /// </summary>
-        /// <param name="array">The one-dimensional Array to sort.</param>
-        /// <param name="comparer">The System.Collections.IComparer implementation to use when comparing elements.-or-null
-        ///    to use the System.IComparable implementation of each element.</param>
-        public static void Sort(this int[] array, IComparer comparer)
+        /// <param name="array">The one-dimensional System.Array to sort.</param>
+        /// <param name="comparison">The System.Comparison to use when comparing elements.</param>
+        public static void Sort(this int[] array, Comparison<int> comparison)
         {
-            ExecuteSort(array, comparer);
+            Merge_Sort(array, comparison);
         }
+
         #endregion
 
         #region Private Methods
 
         /// <summary>
-        /// Executes the sort.
+        /// Sorts the elements in an System.Array using the specified System.Comparison.
         /// </summary>
-        /// <param name="array">The one-dimensional Array to sort.</param>
-        /// <param name="comparer">The System.Collections.IComparer implementation to use when comparing elements.-or-null
-        ///    to use the System.IComparable implementation of each element.</param>
-        private static void ExecuteSort(int[] array, IComparer comparer)
+        /// <param name="array">The one-dimensional System.Array to sort.</param>
+        /// <param name="comparison">The System.Comparison to use when comparing elements.</param>
+        /// <exception cref="System.ArgumentNullException">array is null.-or-comparison is null.</exception>
+        private static void Merge_Sort(int[] array, Comparison<int> comparison)
         {
             if (array == null)
                 throw new ArgumentNullException(nameof(array));
 
-            int[] result = Merge_Sort(array, comparer);
+            if (comparison == null)
+                throw new ArgumentNullException(nameof(comparison));
 
-            Array.Copy(result, array, result.Length);
+            int[] sorted_array = Top_Down_Split_Merge(array, comparison);
+
+            Array.Copy(sorted_array, array, array.Length);
         }
-        private static int[] Merge_Sort(int[] array, IComparer comparer)
+
+        /// <summary>
+        /// Divide the unsorted array into subarrays until subarray size is 1, then merges those subarrays to produce a sorted array.
+        /// </summary>
+        /// <param name="array">The one-dimensional System.Array to sort.</param>
+        /// <param name="comparison">The System.Comparison to use when comparing elements.</param>
+        /// <returns>A sorted array.</returns>
+        private static int[] Top_Down_Split_Merge(int[] array, Comparison<int> comparison)
         {
             if (array.Length == 1)
-            {
                 return array;
-            }
 
             int mid_point = array.Length / 2;
 
-            return Merge(Merge_Sort(array.Take(mid_point).ToArray(), comparer), 
-                Merge_Sort(array.Skip(mid_point).ToArray(), comparer), 
-                comparer);
+            return Merge(Top_Down_Split_Merge(array.Take(mid_point).ToArray(), comparison), 
+                Top_Down_Split_Merge(array.Skip(mid_point).ToArray(), comparison),
+                comparison);
             
         }
 
-        private static int[] Merge(int[] array1, int[] array2, IComparer comparer)
+        /// <summary>
+        /// Merge two arrays into a single sorted one.
+        /// </summary>
+        /// <param name="array1">The first array to merge.</param>
+        /// <param name="array2">The second array to merge.</param>
+        /// <param name="comparison">The System.Comparison to use when comparing elements.</param>
+        /// <returns>A sorted array.</returns>
+        private static int[] Merge(int[] array1, int[] array2, Comparison<int> comparison)
         {
-            int a = 0, b = 0;
+            int array1_index = 0, array2_index = 0;
             int[] merged = new int[array1.Length + array2.Length];
             for (int i = 0; i < array1.Length + array2.Length; i++)
             {
-                if (b < array2.Length && a < array1.Length)
+                if (array2_index < array2.Length && array1_index < array1.Length)
                 {
-                    if (comparer.Compare(array1[a], array2[b]) > 0)
-                        merged[i] = array2[b++];
+                    if (comparison(array1[array1_index], array2[array2_index]) > 0)
+                        merged[i] = array2[array2_index++];
                     else
-                        merged[i] = array1[a++];
+                        merged[i] = array1[array1_index++];
                 }
                 else
                 {
-                    if (b < array2.Length)
-                        merged[i] = array2[b++];
+                    if (array2_index < array2.Length)
+                        merged[i] = array2[array2_index++];
                     else
-                        merged[i] = array1[a++];
+                        merged[i] = array1[array1_index++];
                 }
             }
             return merged;
--- Task2_Console/Program.cs
+++ Task2_Console/Program.cs
@@ -5,50 +5,102 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Task2;
+using static System.Console;
 
 namespace Task2_Console
 {
     class Program
     {
-        public class ReverseComparer : IComparer
-        {
-            // Call CaseInsensitiveComparer.Compare with the parameters reversed.
-            public int Compare(Object x, Object y)
-            {
-                return (new CaseInsensitiveComparer()).Compare(y, x);
-            }
-        }
+        #region Private Fields
 
-        public class DirectComparer : IComparer
-        {
-            // Call CaseInsensitiveComparer.Compare with the parameters reversed.
-            public int Compare(Object x, Object y)
-            {
-                return (new CaseInsensitiveComparer()).Compare(x, y);
-            }
-        }
+        private static Random random = new Random();
+
+        #endregion
+
+        #region Public Methods
 
         static void Main(string[] args)
         {
-            int[] unsorted_array = { 3, 6, 1, 7, 3, 9 };
-            ReverseComparer reverse_comparer = new ReverseComparer();
-            DirectComparer direct_comparer = new DirectComparer();
+            Test_FailToPassNullParameters();
+            Test_ComparePerfomingWithDefaultArraySortOrderByAscending();
+            Test_ComparePerfomingWithLinqOrderByDescending();
+        }
 
-            Display_Array(unsorted_array);
+        static void Test_FailToPassNullParameters()
+        {
+            try
+            {
+                Sorter.Sort(null, null);
+            }
+            catch
+            {
+                WriteLine("Test FailToPassNullParameters - passed");
+                return;
+            }
+            WriteLine("Test FailToPassNullParameters - failed");
+        }
 
-            Console.WriteLine();
+        static void Test_ComparePerfomingWithDefaultArraySortOrderByAscending()
+        {
+            int[] unsorted_array1 = Make_Random_Array(100);
+            int[] unsorted_array2 = (int[])unsorted_array1.Clone();
 
-            Sorter.Sort(unsorted_array, direct_comparer);
+            unsorted_array1.Sort(CompareOrderByAscending);
+            Array.Sort(unsorted_array2);
 
-            Display_Array(unsorted_array);
+            if (unsorted_array1.SequenceEqual(unsorted_array2))
+                WriteLine("Test ComparePerfomingWithDefaultArraySort - passed");
+            else
+                WriteLine("Test ComparePerfomingWithDefaultArraySort - failed");
+        }
+
+        static void Test_ComparePerfomingWithLinqOrderByDescending()
+        {
+            int[] unsorted_array1 = Make_Random_Array(100);
+            int[] unsorted_array2 = (int[])unsorted_array1.Clone();
+
+            unsorted_array2 = unsorted_array1.OrderByDescending(i => i).ToArray();
+            unsorted_array1.Sort(CompareOrderByDescending);
+
+            if (unsorted_array1.SequenceEqual(unsorted_array2))
+                WriteLine("Test ComparePerfomingWithDefaultArraySort - passed");
+            else
+                WriteLine("Test ComparePerfomingWithDefaultArraySort - failed");
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private static int CompareOrderByDescending(int x, int y)
+        {
+            return y.CompareTo(x);
+        }
+
+        private static int CompareOrderByAscending(int x, int y)
+        {
+            return x.CompareTo(y);
+        }
+
+        private static int[] Make_Random_Array(int size)
+        {
+            int[] array = new int[size];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 101);
+            }
+
+            return array;
         }
 
         static private void Display_Array(int[] array)
         {
             for (int i = 0; i < array.Length; i++)
             {
-                Console.Write($"{array[i]} ");
+                Write($"{array[i]} ");
             }
         }
+
+        #endregion
     }
 }
